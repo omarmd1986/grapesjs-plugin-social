ce530544a3fb16b3015820a23607f90d2149441a
Omar <omarmd1986@outlook.com>
2018-04-25 09:57:47 -0700

    Link social block

--- src/blocks.js
+++ src/blocks.js
@@ -1,4 +1,21 @@
 export default (editor, config = {}) => {
-  const bm = editor.BlockManager;
-  // ...
+    const bm = editor.BlockManager;
+
+    var isActive = opt => config.blocks.indexOf(opt) >= 0;
+
+    isActive('facebook') && bm.add(`${config.social}-facebook`, {
+        label: `Facebook`,
+        category: config.socialCategory,
+        content: `<div data-type="flike" class="fb-like" data-href="" data-layout="button_count" data-action="like" data-size="large" data-show-faces="true" data-share="true" style="min-height:50px; width:100px;"></div>`,
+        attributes: {class: 'fa fa-plus-square'}
+    });
+    
+    
+    isActive('link') && bm.add(`${config.social}-link`, {
+        label: `Social Link`,
+        category: config.socialCategory,
+        content: `<a href="#" class="fa fa-facebook" data-type="${config.social}-link"></a>`,
+        attributes: {class: 'fa fa-twitter-square'}
+    });
+
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import grapesjs from 'grapesjs';
 import loadComponents from './components';
 import loadBlocks from './blocks';
+import {style} from './consts';
 
 export default grapesjs.plugins.add('gjs-social', (editor, opts = {}) => {
-  const options = { ...{
-    // default options
-  },  ...opts };
+    const options = {...{
+                blocks: ['facebook', 'link'],
 
-  // Add components
-  loadComponents(editor, options);
+                prefix: 'social',
 
-  // Add blocks
-  loadBlocks(editor, options);
+                style: style,
+                
+                socialCategory: 'Social'
+        }, ...opts};
+
+    // Add components
+    loadComponents(editor, options);
+
+    // Add blocks
+    loadBlocks(editor, options);
+
+    // Adding the font awesom
+    editor.on('load', () => {
+        editor.addComponents('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">');
+        editor.addComponents(options.style);
+    });
 });
